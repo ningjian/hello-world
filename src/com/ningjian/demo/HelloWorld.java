128e7d1691b4bec29f9b2b143898cfeded8ee0f3
ningjian <ningjian188@outlook.com>
2017-07-05 00:24:52 +0800

    Initial commit

---
+++ src/com/ningjian/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ningjian.demo;
+
+/**
+ * Created by nj on 2017/7/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+       System.out.println("hello,the world!");
+    }
+}
